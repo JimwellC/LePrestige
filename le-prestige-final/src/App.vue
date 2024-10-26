ec62dbe81ce1b4235ed2ce2356cecad725124a29
jaibwii <jbrwoj32@gmail.com>
2024-10-26 13:22:43 +0800

    FINALVUE

--- le-prestige-final/src/App.vue
+++ le-prestige-final/src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    
+    <nav class="navbar navbar-expand-lg bg-white shadow-lg">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <router-link class="navbar-brand" to="/" exact>
+                    Le Prestige
+                </router-link>
+                
+
+                <div class="d-lg-none">
+                    <button type="button" class="custom-btn btn btn-danger" data-bs-toggle="modal" data-bs-target="#BookingModal">Reservation</button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav mx-auto">
+    <li class="nav-item">
+      <router-link class="nav-link "  to="/" exact active-class="active">Home</router-link>
+    </li>
+
+    <li class="nav-item">
+      <router-link class="nav-link " to="/Menu" active-class="active">Menu</router-link>
+    </li>
+
+    <li class="nav-item">
+      <router-link class="nav-link " to="/Contact" active-class="active">Contact</router-link>
+    </li>
+  </ul>
+</div>
+
+                <div class="d-none d-lg-block">
+                    <button type="button" class="custom-btn btn btn-danger" data-bs-toggle="modal" data-bs-target="#BookingModal">Reservation</button>
+                </div>
+
+            </div>
+        </nav>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style >
+@import './assets/css/tooplate-crispy-kitchen.css';
 </style>
--- le-prestige-final/src/main.js
+++ le-prestige-final/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/css/bootstrap.min.css'
+import '@/assets/css/bootstrap-icons.css'
+import '@/assets/css/tooplate-crispy-kitchen.css'
+
+// Import Bootstrap JavaScript for any JS-based Bootstrap components
+import 'bootstrap';
 
 Vue.config.productionTip = false
 
